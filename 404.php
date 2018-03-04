c1a2b25dc7bf1e269a13ca698c5193e105368a27
Chris Watkins <watkin48@purdue.edu>
2018-03-04 18:10:51 -0500

    Adding first attempt at 404 page

--- 404.php
+++ 404.php
@@ -6,19 +6,19 @@ $metaTitle = 'Page Not Found - Chris Watkins, UX Researcher & Designer';
 $metaDescription = '';
 
 // Include specific header and footer style variable in here
-$headerType = 'white-header';
-$logoColor = "black-logo";
-$navColor = 'black-nav';
-$activeNav = '1';
-$responsiveNavRef = 'whitebg-res-nav';
-$footerType = 'black-footer';
+$headerType = 'no-bg-header';
+$logoColor = "white-logo";
+$navColor = 'white-nav';
+$activeNav = '0';
+$responsiveNavRef = 'dark-res-nav';
+$footerType = 'white-footer';
 
 ?>
 <!DOCTYPE html>
 <html>
 <?php include 'content/elements/head.php'; ?>
 
-<body>
+<body class="not-found">
 
 <div id="content">
 
--- content/pages/404.php
+++ content/pages/404.php
@@ -1,10 +1,60 @@
 <div class="page-content">
-<section class="top-pad-200 top-about-content"> <!-- about-gradient">-->
+<section class="top-pad-200 main-top-section top-about-content"> <!-- about-gradient">-->
 	<div class="row">
+		<div class="small-5 show-for-medium columns">
+			
+		</div>
 		<div class="small-24 medium-19 columns">
-			<p class="capitalized-title-desc no-margin-bottom" id="content-top-indicator"><span>oh dear</span></p>
-			<h1 style="margin-bottom: .5rem !important;">Page not found</h1>
-		</div>	
+			<p class="capitalized-title-desc text-white no-margin-bottom" id="content-top-indicator"><span>page not found</span></p>
+			<h1 class="text-white about-page" style="margin-bottom: .5rem !important;">Hello there... <span class="light">It seems this page doesn't exist.</span></h1>
+			<div class="row about-me-section options-section">
+				<div class="small-24 large-7 columns">
+					<h2 class="text-white about-subsection">So, where next?</h2>
+				</div>
+				<div class="small-24 large-17 columns">
+					<div class="row">
+						<div class="small-24 columns">
+							<a href="/#work-anchor" class="about-page-bottom-option work-option fadeInQuick">
+								<div class="color-block-overlay"></div>
+								<div class="gradient-block-overlay"></div>
+								<div class="hovered-line" style="background-color: #FF0086;"></div>
+								<div class="image-underlay" style="background-image: url('img/project-items/lyft.jpg');"></div>
+								<div class="row">
+									<div class="small-17 medium-14 columns">
+										<h3 class="text-white">View my work</h3>
+										<p class="text-white">Check out some of my work, with a range of client, university, and personal projects. </p>
+									</div>
+								</div>
+							</a>
+							<a title="Chris Watkins - Resume, Spring 2018" href="https://drive.google.com/file/d/1ZrraajkHZyLWjNm1HHFG0gvdtMrQYr9o/view?usp=sharing" class="about-page-bottom-option resume-option fadeInQuick">
+								<div class="color-block-overlay"></div>
+								<div class="gradient-block-overlay"></div>
+								<div class="hovered-line" style="background-color: rgba(47, 130, 246, 1.00);"></div>
+								<div class="image-underlay" style="background-image: url('img/chris/resumetemplate-small.jpg');"></div>
+								<div class="row">
+									<div class="small-17 medium-14 columns">
+										<h3>See my résumé</h3>
+										<p class="text-white">View or download a history of my relevant experience and education, and my skillset.</p>
+									</div>
+								</div>
+							</a>
+							<a href="mailto:chris@crwatkins.com?Subject=Hey,%20Chris!" class="about-page-bottom-option contact-option fadeInQuick">
+								<div class="color-block-overlay"></div>
+								<div class="gradient-block-overlay"></div>
+								<div class="hovered-line" style="background-color: #cfb2b1;"></div>
+								<div class="image-underlay" style="background-image: url('img/chris/headshot-small.jpg');"></div>
+								<div class="row">
+									<div class="small-17 medium-14 columns">
+										<h3>Get in touch</h3>
+										<p>Want to discuss a project, grab a coffee, or just say hello? Feel free to drop me an email!</p>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </section>
 </div>

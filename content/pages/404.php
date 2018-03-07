6c4a73d4496d0a833a3c0223be035eb6b1166b1a
Chris Watkins <watkin48@purdue.edu>
2018-03-07 10:26:08 -0500

    Minor style change to about / 404 page options on mobile

--- content/pages/404.php
+++ content/pages/404.php
@@ -16,11 +16,11 @@
 						<div class="small-24 columns">
 							<a href="/#work-anchor" class="about-page-bottom-option work-option fadeInQuick">
 								<div class="color-block-overlay"></div>
-								<div class="gradient-block-overlay"></div>
+								<div class="gradient-block-overlay show-for-medium"></div>
 								<div class="hovered-line" style="background-color: #FF0086;"></div>
-								<div class="image-underlay" style="background-image: url('img/project-items/lyft.jpg');"></div>
+								<div class="image-underlay show-for-medium" style="background-image: url('img/project-items/lyft.jpg');"></div>
 								<div class="row">
-									<div class="small-17 medium-14 columns">
+									<div class="small-24 medium-14 columns">
 										<h3 class="text-white">View my work</h3>
 										<p class="text-white">Check out some of my work, with a range of client, university, and personal projects. </p>
 									</div>
@@ -28,11 +28,11 @@
 							</a>
 							<a title="Chris Watkins - Resume, Spring 2018" href="https://drive.google.com/file/d/1ZrraajkHZyLWjNm1HHFG0gvdtMrQYr9o/view?usp=sharing" class="about-page-bottom-option resume-option fadeInQuick">
 								<div class="color-block-overlay"></div>
-								<div class="gradient-block-overlay"></div>
+								<div class="gradient-block-overlay show-for-medium"></div>
 								<div class="hovered-line" style="background-color: rgba(47, 130, 246, 1.00);"></div>
-								<div class="image-underlay" style="background-image: url('img/chris/resumetemplate-small.jpg');"></div>
+								<div class="image-underlay show-for-medium" style="background-image: url('img/chris/resumetemplate-small.jpg');"></div>
 								<div class="row">
-									<div class="small-17 medium-14 columns">
+									<div class="small-24 medium-14 columns">
 										<h3>See my résumé</h3>
 										<p class="text-white">View or download a history of my relevant experience and education, and my skillset.</p>
 									</div>
@@ -40,11 +40,11 @@
 							</a>
 							<a href="mailto:chris@crwatkins.com?Subject=Hey,%20Chris!" class="about-page-bottom-option contact-option fadeInQuick">
 								<div class="color-block-overlay"></div>
-								<div class="gradient-block-overlay"></div>
+								<div class="gradient-block-overlay show-for-medium"></div>
 								<div class="hovered-line" style="background-color: #cfb2b1;"></div>
-								<div class="image-underlay" style="background-image: url('img/chris/headshot-small.jpg');"></div>
+								<div class="image-underlay show-for-medium" style="background-image: url('img/chris/headshot-small.jpg');"></div>
 								<div class="row">
-									<div class="small-17 medium-14 columns">
+									<div class="small-24 medium-14 columns">
 										<h3>Get in touch</h3>
 										<p>Want to discuss a project, grab a coffee, or just say hello? Feel free to drop me an email!</p>
 									</div>
